5a8c41837de39b962c08173a1ae900b9d03ef658
Alain Rizzi <s350714@studenti.polito.it>
2026-05-30 00:43:09 +0300

    few setups

---
+++ server/dao.js
@@ -0,0 +1,4 @@
+/* Data Access Object (DAO) module for accessing films data */
+
+import dayjs from "dayjs";
+import db from "./db.js";
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 // imports
 import express from "express";
+import morgan from 'morgan'; // logging middleware
+import cors from 'cors'; // CORS middleware
+import {check, validationResult} from 'express-validator'; // validation middleware
 
 // init express
 const app = new express();
+app.use(morgan('dev'));
+app.use(express.json());
 const port = 3001;
+
+/** Set up and enable Cross-Origin Resource Sharing (CORS) **/
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+};
+app.use(cors(corsOptions));
 
 // activate the server
 app.listen(port, () => {
