0c4c5de4d7bbdbfdb864485d682e742427e602cd
Alain Rizzi <s350714@studenti.polito.it>
2026-06-01 19:58:12 +0300

    added authentication strategy and session

--- server/dao.js
+++ server/dao.js
@@ -2,3 +2,41 @@
 
 import dayjs from "dayjs";
 import db from "./db.js";
+import { User, Station, Line, Game, Step } from "./models.js";
+import crypto from "crypto";
+
+export const getUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT id, username, name FROM users WHERE id = ?';
+        db.get(sql, [id], (err, row) => {
+            if (err) reject(err);
+            else if (row === undefined) resolve(false);
+            else resolve({ id: row.id, username: row.username, name: row.name });
+        });
+    });
+};
+
+export const getUserByCredentials = (username, password) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM users WHERE username = ?';
+        db.get(sql, [username], (err, row) => {
+            if (err) {
+                reject(err);
+            } else if(row === undefined) {
+                resolve(false); // no user with this username
+            } 
+            else {
+                const user = new User(row.id, row.username, row.name);
+
+                crypto.scrypt(password, row.salt, 16, function(err, hashedPassword){
+                    if(err) reject(err);
+                    if (!crypto.timingSafeEqual(Buffer.from(row.hash, 'hex'), hashedPassword)) {
+                        resolve(false); // password does not match
+                    } else {
+                        resolve(user); // success: username and password match
+                    }
+                });
+            }
+        }); 
+    });
+};
--- server/index.js
+++ server/index.js
@@ -17,6 +17,56 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+/*** Passport ***/
+
+/** Authentication-related imports **/
+import passport from 'passport';                              // authentication middleware
+import LocalStrategy from 'passport-local';                   // authentication strategy (username and password)
+import { getUserByCredentials, getUserById } from './dao.js';
+
+/** Set up authentication strategy to search in the DB a user with a matching password.
+ * The user object will contain other information extracted by the method getUserByCredentials() (i.e., id, username, name).
+ **/
+passport.use(new LocalStrategy(async function verify(username, password, callback) {
+    const user = await getUserByCredentials(username, password)
+    if(!user)
+      return callback(null, false, 'Incorrect username or password');
+
+    return callback(null, user); // NOTE: user info in the session (all fields returned by userDao.getUserByCredentials(), i.e, id, username, name)
+}));
+
+// Serializing in the session the user object given from LocalStrategy(verify).
+passport.serializeUser(function (user, callback) { // this user is id + username + name
+    callback(null, user);
+});
+
+// Starting from the data in the session, we extract the current (logged-in) user.
+passport.deserializeUser(function (user, callback) { // this user is id + username + name
+    getUserById(user.id)
+        .then(u => callback(null, u))
+        .catch(err => callback(err, null));
+});
+
+
+/** Creating the session */
+import session from 'express-session';
+
+app.use(session({
+  secret: "This is a very secret information used to initialize the session!",
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.authenticate('session'));
+
+
+/** Defining authentication verification middleware **/
+const isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()) {
+      return next();
+    }
+    return res.status(401).json({error: 'Not authorized'});
+}
+
 // activate the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
